98e48785c523b1e5a2f1cc2705cc77072398eb9e
kjjkjjzyayufqza <kjjkjjzyayufqza@yahoo.com>
2021-02-10 13:34:11 +0800

    hi

--- live2d/assets/waifu-tips.js
+++ live2d/assets/waifu-tips.js
@@ -57,6 +57,7 @@ live2d_settings['waifuTipsSize']        = '250x70';     // 提示框大小，例
 live2d_settings['waifuFontSize']        = '12px';       // 提示框字体，例如 '12px', '30px'
 live2d_settings['waifuToolFont']        = '14px';       // 工具栏字体，例如 '14px', '36px'
 live2d_settings['waifuToolLine']        = '20px';       // 工具栏行高，例如 '20px', '36px'
+live2d_settings['waifuFontColor']        = "blue";       // 设置颜色自己加
 live2d_settings['waifuToolTop']         = '0px'         // 工具栏顶部边距，例如 '0px', '-60px'
 live2d_settings['waifuMinWidth']        = '768px';      // 面页小于 指定宽度 隐藏看板娘，例如 'disable'(禁用), '768px'
 live2d_settings['waifuEdgeSide']        = 'left:0';     // 看板娘贴边方向，例如 'left:0'(靠左 0px), 'right:30'(靠右 30px)
